076c990225e25a1769771d68f3efda7982ca21c5
Rail Rahimov <rahimovrail497@gmail.com>
2023-12-09 16:00:31 +0300

    added

--- src/main/java/com/rahimov/demo/FirstMicroserviceApplication.java
+++ src/main/java/com/rahimov/demo/FirstMicroserviceApplication.java
@@ -2,12 +2,22 @@ package com.rahimov.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class FirstMicroserviceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FirstMicroserviceApplication.class, args);
+	}
+
+	@GetMapping("/hello")
+	@ResponseBody
+	public String hello () {
+		return "Hello";
 	}
 
 }
